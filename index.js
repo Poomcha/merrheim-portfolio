ad64437c722f4f115302b182fea909cde0bb65a7
poomcha <poompoomcha@gmail.com>
2023-11-13 23:57:05 +0100

    Base quasi fini.

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 // --------------
 //     UTILS
 // --------------
-// Window size tracking
-const getWindowDimensions = () => ({
-  width: window.innerWidth,
-  height: window.innerHeight,
-});
 // Variables
 const datas = {
   breakpoints: {
@@ -16,6 +11,53 @@ const datas = {
     xl: 960,
     xxl: 1120,
   },
+  text: {
+    nav: {
+      accueil: 'ACCUEIL',
+      aproposdemoi: 'A PROPOS DE MOI',
+      projets: 'PROJETS',
+      contact: 'CONTACT',
+    },
+  },
+  elements: {
+    sound_toggler: document.querySelector('.sound'),
+  },
+};
+// Window size tracking
+const getWindowDimensions = () => ({
+  width: window.innerWidth,
+  height: window.innerHeight,
+});
+// Navigation labels visibility
+const showNavLabels = () => {
+  const label_accueil = document.querySelector('#label-accueil');
+  const label_aproposdemoi = document.querySelector('#label-aproposdemoi');
+  const label_projets = document.querySelector('#label-projets');
+  const label_contact = document.querySelector('#label-contact');
+
+  if (window.innerWidth > datas.breakpoints.s) {
+    label_accueil.innerHTML = datas.text.nav.accueil;
+    label_aproposdemoi.innerHTML = datas.text.nav.aproposdemoi;
+    label_projets.innerHTML = datas.text.nav.projets;
+    label_contact.innerHTML = datas.text.nav.contact;
+  } else {
+    [label_accueil, label_aproposdemoi, label_projets, label_contact].forEach(
+      (label) => (label.innerHTML = '')
+    );
+  }
+};
+// Toggle sound
+const toggleSound = () => {
+  state.sound.on = !state.sound.on;
+};
+// Toggle sound image
+const toggleSoundImage = () => {
+  const img_toggler = document.querySelector('#sound-toggler');
+  if (state.sound.on) {
+    img_toggler.setAttribute('src', './public/images/icons/volume-on.svg');
+  } else {
+    img_toggler.setAttribute('src', './public/images/icons/volume-off.svg');
+  }
 };
 // --------------
 //     STATE
@@ -33,9 +75,19 @@ const state = {
 //     INIT
 // --------------
 window.addEventListener('load', () => {
+  getWindowDimensions();
+  showNavLabels();
+  toggleSoundImage();
   // Handle resize
   window.addEventListener('resize', () => {
     state.window.width = getWindowDimensions().width;
     state.window.height = getWindowDimensions().height;
+    // Handle navigation
+    showNavLabels();
+  });
+  // Handle sound toggle
+  datas.elements.sound_toggler.addEventListener('click', (e) => {
+    toggleSound();
+    toggleSoundImage();
   });
 });
